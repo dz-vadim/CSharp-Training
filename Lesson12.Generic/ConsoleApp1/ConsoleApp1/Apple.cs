70d055f228f8502a2b7744aa2a61515ed202eb9a
dz-vadim <46889822+dz-vadim@users.noreply.github.com>
2020-08-10 15:48:40 +0300

    Add files via upload

---
+++ Lesson12.Generic/ConsoleApp1/ConsoleApp1/Apple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Apple<T>: Product<T>
+    {
+        public Apple(string name, T volume, T energy)
+            : base( name, volume, energy)
+        {
+
+        }
+    }
+}
