6df0e3f986a1dfd81051720766478ae075d38805
dz-vadim <46889822+dz-vadim@users.noreply.github.com>
2020-08-10 14:38:53 +0300

    Add files via upload

---
+++ Lesson11/Lesson11/Apple.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    public class Apple : Product
+    {
+        public Apple(string name, int calorie, int volume) : base(name, calorie, volume)
+        {
+
+        }
+    }
+}
