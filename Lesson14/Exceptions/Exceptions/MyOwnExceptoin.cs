39c7b3c4c7f439fc624c8d4477f70d202bf723aa
dz-vadim <46889822+dz-vadim@users.noreply.github.com>
2020-08-10 17:42:39 +0300

    Add files via upload

---
+++ Lesson14/Exceptions/Exceptions/MyOwnExceptoin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Exceptions
+{
+    class MyOwnExceptoin : Exception
+    {
+        public MyOwnExceptoin() : base ("This my exception")
+        {
+        }
+        public MyOwnExceptoin(string messege):base(messege)
+        {
+
+        }
+    }
+}
